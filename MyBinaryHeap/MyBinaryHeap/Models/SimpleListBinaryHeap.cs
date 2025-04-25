2bd5c3bbc6a9e73592cc532f0ca7de125435f348
EBuravskiy <elric0sphinx@gmail.com>
2025-04-25 17:40:01 +0500

    Create simple binary heap

---
+++ MyBinaryHeap/MyBinaryHeap/Models/SimpleListBinaryHeap.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+namespace MyBinaryHeap.Models
+{
+    public class SimpleListBinaryHeap<T> : IEnumerable
+        where T : IComparable
+    {
+        private List<T> items;
+        public int Count => items.Count;
+
+        public SimpleListBinaryHeap()
+        {
+            items = new List<T>();
+        }
+
+        public SimpleListBinaryHeap(List<T> items) : this()
+        {
+            this.items.AddRange(items);
+            for (int i = Count; i >= 0; i--)
+            {
+                Sort(i);
+            }
+        }
+
+        public T? Peek()
+        {
+            if (Count > 0)
+            {
+                return items[0];
+            }
+            return default(T);
+        }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+            int currentIndex = Count - 1;
+            int parentIndex = GetParentIndex(currentIndex);
+
+            while (currentIndex > 0 && items[currentIndex].CompareTo(items[parentIndex]) > 0)
+            {
+                Swap(currentIndex, parentIndex);
+                currentIndex = parentIndex;
+                parentIndex = GetParentIndex(currentIndex);
+            }
+        }
+
+        private int GetParentIndex(int currentIndex)
+        {
+            return (currentIndex - 1) / 2;
+        }
+
+        private void Swap(int currentIndex, int parentIndex)
+        {
+            T temp = items[currentIndex];
+            items[currentIndex] = items[parentIndex];
+            items[parentIndex] = temp;
+
+        }
+
+        public T Pop()
+        {
+            T result = default(T);
+            if (Count > 0)
+            {
+                int index = 0;
+                result = items[index];
+                items[index] = items[Count - 1];
+                items.RemoveAt(Count - 1);
+                Sort(index);
+            }
+            return result;
+        }
+
+        private void Sort(int currentIndex)
+        {
+            int maxIndex;
+            int leftIndex;
+            int rightIndex;
+            while (currentIndex < Count)
+            {
+                maxIndex = currentIndex;
+                leftIndex = currentIndex * 2 + 1;
+                rightIndex = currentIndex * 2 + 2;
+                if (leftIndex < Count && items[leftIndex].CompareTo(items[maxIndex]) > 0)
+                {
+                    maxIndex = leftIndex;
+                }
+                if (rightIndex < Count && items[rightIndex].CompareTo(items[maxIndex]) > 0)
+                {
+                    maxIndex = rightIndex;
+                }
+                if (currentIndex == maxIndex)
+                {
+                    break;
+                }
+                Swap(currentIndex, maxIndex);
+                currentIndex = maxIndex;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            while (Count > 0)
+            {
+                yield return Pop();
+            }
+        }
+    }
+}
---
+++ MyBinaryHeap/MyBinaryHeap/Program.cs
@@ -0,0 +1,41 @@
+﻿using MyBinaryHeap.Models;
+using System.Diagnostics;
+
+namespace MyBinaryHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            var rnd = new Random();
+            var startItems = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                startItems.Add(rnd.Next(-10, 10));
+            }
+
+            timer.Start();
+            SimpleListBinaryHeap<int> simpleHeap = new SimpleListBinaryHeap<int>(startItems);
+            timer.Stop();
+            Console.WriteLine("Первоначальная инициализация " + timer.Elapsed.ToString());
+
+            timer.Restart();
+            for (int i = 0; i < 10; i++)
+            {
+                simpleHeap.Push(rnd.Next(-10, 10));
+            }
+            timer.Stop();
+            Console.WriteLine("Добавление элементов " + timer.Elapsed.ToString());
+
+            timer.Restart();
+            foreach (var item in simpleHeap)
+            {
+                Console.WriteLine(item);
+            }
+            timer.Stop();
+            Console.WriteLine("Вывод элементов " + timer.Elapsed.ToString());
+        }
+    }
+}
